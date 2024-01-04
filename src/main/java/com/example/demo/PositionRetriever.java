70880eff16e12242f75a62db9e8ba3bdd05bce38
윤영우 <macair1995@naver.com>
2024-01-05 00:23:07 +0900

    Add WebSocket feature

--- src/main/java/com/example/demo/PositionRetriever.java
+++ src/main/java/com/example/demo/PositionRetriever.java
@@ -2,10 +2,14 @@ package com.example.demo;
 
 import com.example.demo.domain.Aircraft;
 import com.example.demo.repository.AircraftRepository;
+import com.example.demo.websocket.WebSocketHandler;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -13,13 +17,28 @@ import java.util.function.Consumer;
 @Configuration
 public class PositionRetriever {
     private final AircraftRepository repository;
+    private final WebSocketHandler handler;
 
     @Bean
     Consumer<List<Aircraft>> retrieveAircraftPositions() {
         return acList -> {
           repository.deleteAll();
           repository.saveAll(acList);
-          repository.findAll().forEach(System.out::println);
+          sendPositions();
         };
+    }
+
+    private void sendPositions() {
+        if (repository.count() > 0) {
+            for (WebSocketSession sessionInList : handler.getSessionList()) {
+                try {
+                    sessionInList.sendMessage(
+                            new TextMessage(repository.findAll().toString())
+                    );
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
